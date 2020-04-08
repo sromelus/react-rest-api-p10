49e24b98b2b8ff2bb9d00735de549a51ea5e71fe
sromelus <46148909+sromelus@users.noreply.github.com>
2020-04-07 21:29:35 -0400

    update logic in create course page

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -5,14 +5,137 @@ export default class CreateCourse extends Component {
   constructor(){
     super()
     this.state = {
+      title: "",
+      description: "",
+      estimatedTime: "",
+      materialsNeeded: "",
+      errors: []
     }
   }
+
+  cancel = (e) => {
+    e.preventDefault()
+    this.props.history.push('/');
+  }
+
+  handleTitleChange = (e) => {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  handleDescriptionChange = (e) => {
+    this.setState({
+      description: e.target.value
+    })
+  }
+
+  handleEstimatedTimeChange = (e) => {
+    this.setState({
+      estimatedTime: e.target.value
+    })
+  }
+
+  handleMaterialsNeededChange = (e) => {
+    this.setState({
+      materialsNeeded: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { title, description, estimatedTime, materialsNeeded } = this.state
+
+    const course = { title, description, estimatedTime, materialsNeeded }
+
+    const encodedCredentials = btoa(`j@j.com:james`);
+
+      fetch('http://localhost:5000/api/courses', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          // 'Authorization': `Basic ${encodedCredentials}`
+        },
+        body: JSON.stringify(course)
+      })
+      .then(res => {
+        if (res.status === 201) {
+          this.props.history.push('/');
+          return [];
+        } else if (res.status === 400 || res.status === 401, res.status === 403){
+            return res.json()
+            .then(body => {
+              this.setState( prevState => ({
+                errors: body.message
+              }))
+            })
+        } else {
+          let errorMessage = `${res.status}(${res.statusText})`
+          const error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
+
   render(){
     return (
-      <div className="">
+      <div className="bounds course--detail">
+        <h1>Create Course</h1>
+        <ErrorsDisplay errors={this.state.errors}/>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <div className="grid-66">
+              <div className="course--header">
+                <h4 className="course--label">Course</h4>
+                <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
+                    onChange={this.handleTitleChange} value={this.state.title}/></div>
+                <p>By Joe Smith</p>
+              </div>
+              <div className="course--description">
+                <div><textarea id="description" name="description" className="" placeholder="Course description..." onChange={this.handleDescriptionChange} value={this.state.description}></textarea></div>
+              </div>
+            </div>
+            <div className="grid-25 grid-right">
+              <div className="course--stats">
+                <ul className="course--stats--list">
+                  <li className="course--stats--list--item">
+                    <h4>Estimated Time</h4>
+                    <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
+                        placeholder="Hours" onChange={this.handleEstimatedTimeChange} value={`${this.state.estimatedTime}`}/></div>
+                  </li>
+                  <li className="course--stats--list--item">
+                    <h4>Materials Needed</h4>
+                    <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." onChange={this.handleMaterialsNeededChange} value={this.state.materialsNeeded}></textarea></div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary" onClick={this.cancel}>Cancel</button></div>
+          </form>
+        </div>
       </div>
     );
   }
+}
+
+const ErrorsDisplay = ({ errors }) => {
+  let errorsDisplay = null;
+
+  if (errors.length) {
+    errorsDisplay = (
+      <div>
+        <h2 className="validation--errors--label">Validation errors</h2>
+        <div className="validation-errors">
+          <ul>
+            {errors.map((error, i) => <li key={i}>{error}</li>)}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  return errorsDisplay;
 }
 
 // CreateCourse - This component provides the "Create Course" screen by rendering a form that allows a user to create a new course. The component also renders a "Create Course" button that when clicked sends a POST request to the REST API's /api/courses route. This component also renders a "Cancel" button that returns the user to the default route (i.e. the list of courses).
